dce50fb67c4db0f4d38f908d2685b743b5a4d936
Calev Chandra <>
2021-05-18 02:07:45 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Coding Test - Open Restaurants
+
+## Specification
+
+Your task is to read a JSON file that contains data describing the opening
+times of a number of restaurants, and then provide a method for querying
+which of the restaurants are open at a specified date and time.
+
+A simple code and test skeleton written in JavaScript/Mocha is provided to help
+get you started, but you may write your solution in any language.
+
+## Test Data
+
+Test data can be found in the `restaurant_data.json` file. Inspect this file
+to determine the kind of input data that your code should be able to handle.
+
+All the data in `restaurant_data.json` are correct and valid. You may add
+additional entries for testing purposes, but please do not alter the existing
+content.
+
+## Additional Information
+
+- All dates and times can be assumed to be in the same time zone
+- The input file can be assumed to be well-formed and contain no invalid data
+- Remember to write tests for your solution, particularly for any edge cases
+  that could occur in the input data
+
+## Deliverable
+
+The deliverable is the source code and files required to build and run your
+solution. Either a public git repository, e.g. GitHub, GitLab or BitBucket, or
+a zip archive.
+
+Please also provide a README that contains the following:
+
+- Any instructions needed to boot and test your solution
+- A technical overview of the implementation, including:
+  - A summary of its architecture.
+  - Technical justification of key design decisions with comparison to other
+    possible approaches.
+  - Implementation notes that would help orient a coder new to the code
+  - Any areas of the design or implementation that could be improved further or
+    that you would like specific feedback on.
+
+## Tips
+
+- A simple and straightforward design is preferred. Avoid unnecessary
+  complexity.
+- Using a strict code linter and automatic formatter is recommended
+- Remove debug or other unnecessary code before submitting
+- Don't forget the tests.
+
+## Questions
+
+If you have any questions about the assignment please be in touch with a member
+of the HeartLab team and we’ll do our best to help in whatever way we
+can.
+
+Good luck!
---
+++ app/restaurants.js
@@ -0,0 +1,147 @@
+const fs = require("fs");
+const luxon = require("luxon");
+
+/**
+ * This class takes the name of a JSON file containing details on opening hours
+ * for a number of restaurants. It parses the contents of this file and then
+ * provides a method for querying which restaurants are open at a specified date
+ * and time. The input JSON file can be assumed to contain correctly formatted
+ * data.
+ *
+ * All dates and times can be assumed to be in the same time zone.
+ */
+class Restaurants {
+  constructor(jsonFilename) {
+    const jsonData = JSON.parse(fs.readFileSync(jsonFilename));
+    this.restaurants = [];
+    let daysAsNumberFormat = [];
+
+    for (var value of jsonData.restaurants) {
+      const arrOpeningHours = value.opening_hours.split(";")
+
+      daysAsNumberFormat = [];
+      for (var val of arrOpeningHours){
+        val = val.trim();
+        let firstDay = val.substring(0, 3);
+
+        let timesOpen;
+        let arrTimesOpen;
+
+        if (val.charAt(3) === "-"){
+          let lastDay = val.substring(4, 7);
+          timesOpen = val.substring(7, val.length);
+          arrTimesOpen = this.splitStringIntoArrayAndTrim(timesOpen);
+          daysAsNumberFormat.push({ weekdays: this.getDays(firstDay, lastDay), times: arrTimesOpen });
+          
+        } else{
+          timesOpen = val.substring(4, val.length);
+          arrTimesOpen = this.splitStringIntoArrayAndTrim(timesOpen);
+          daysAsNumberFormat.push({ weekdays: this.getDays(firstDay, null), times: arrTimesOpen});
+        }
+      }
+      this.restaurants.push({ name: value.name, opening_hours: value.opening_hours, opening_times:  daysAsNumberFormat });
+    }
+  }
+  splitStringIntoArrayAndTrim (strValue){
+    let arrayVal = strValue.split("-");
+    arrayVal[0] = arrayVal[0].trim();
+    arrayVal[1] = arrayVal[1].trim();
+    return arrayVal;
+  }
+
+  convertTime12to24(time12h){
+    const [time, modifier] = time12h.split(' ');
+
+    let [hours, minutes] = time.split(':');
+
+    if (hours === '12') {
+      hours = '00';
+    }
+
+    if (modifier.toLowerCase() === 'pm') {
+      hours = parseInt(hours, 10) + 12;
+    }
+
+     return { hours: parseInt(hours), minutes: isNaN(minutes) ? 0 : parseInt(minutes)};
+  }
+
+  getDays(firstDay, lastDay){
+    let daysOpen = [];
+    const firstDayAsNum = this.getDayAsNumber(firstDay);
+    if (lastDay) {
+      const lastDayAsNum = this.getDayAsNumber(lastDay);
+      for (let i = firstDayAsNum; i <= lastDayAsNum; i++){
+        daysOpen.push(i);
+      }
+    } else {
+      daysOpen.push(firstDayAsNum);
+    }
+    return daysOpen;
+  }
+
+  getDayAsNumber(day){
+    let dayAsNumber;
+    switch (day.toLowerCase()) {
+      case "mon":
+        dayAsNumber = 0;
+        break;
+      case "tue":
+        dayAsNumber = 1;
+        break;
+      case "wed":
+        dayAsNumber = 2;
+        break;
+      case "thu":
+        dayAsNumber = 3;
+        break;
+      case "fri":
+        dayAsNumber = 4;
+        break;
+      case "sat":
+        dayAsNumber = 5;
+        break;
+      case "sun":
+        dayAsNumber = 6;
+    }
+    return dayAsNumber;
+  }
+
+  
+
+  /**
+   * Finds the restaurants open at the specified time.
+   *
+   * @param {luxon.DateTime} time
+   * @returns {Array<string>} The names of the restaurants open at the specified
+   * time. The order of the elements in this array is alphabetical.
+   */
+  getRestaurantsOpenAt(time) {
+    let results = []
+    const timeStamp = time.ts;
+    const date = new Date(timeStamp);
+    const days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+    
+    const weekdayRestOpen = this.getDayAsNumber(days[date.getDay()]);
+
+    for (const rest of this.restaurants) {
+      for (const openingTimes of rest.opening_times){
+        if (openingTimes.weekdays.includes(weekdayRestOpen)) {
+          const openTimeTo24hr = this.convertTime12to24(openingTimes.times[0]);
+          const closeTimeTo24hr = this.convertTime12to24(openingTimes.times[1]);
+
+          const start = parseInt(openTimeTo24hr.hours * 60 +  openTimeTo24hr.minutes);
+          const end = parseInt(closeTimeTo24hr.hours * 60 + closeTimeTo24hr.minutes);
+          
+          const timeToCheck = time.c.hour * 60 + time.c.minute;
+            if (timeToCheck >= start && timeToCheck <= end){
+              results.push(rest.name);
+            }
+        }
+      }
+    }
+
+    return results;
+  }
+}
+
+module.exports = Restaurants;
